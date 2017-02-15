fe899e917e150f907b261797f023ef1035aed148
Bharadwaj Singh Bondili <b.bharadwaj@outlook.com>
2017-02-16 00:27:01 +0530

    SecondCommit

--- src/com/project/tests/BasicClass.java
+++ src/com/project/tests/BasicClass.java
@@ -1,5 +1,9 @@
 package com.project.tests;
 
 public class BasicClass {
-
+  public static void main(String[] args) {
+	System.out.println("Hello-World");
+	System.out.println("");
+	System.out.println("");
+}
 }
